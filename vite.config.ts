0f8bf4efcf703dad389b376ac7ecc1ee9c8b2fe9
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-08-10 10:23:26 -0500

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from "node:path";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), cloudflare()],
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "./client"),
