5cb1c03ef703be55fbe7b75aedf3090125df342c
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-08-13 19:01:21 -0500

    add shadow to DialogContent popup for improved visibility

--- client/components/DialogContent.tsx
+++ client/components/DialogContent.tsx
@@ -9,7 +9,7 @@ export const DialogContent = ({
 		<BaseDialog.Backdrop className="fixed inset-0 bg-black opacity-30 transition-opacity duration-200 sm:flex sm:justify-center data-[starting-style]:opacity-0 data-[ending-style]:opacity-0 dark:opacity-70" />
 		<BaseDialog.Popup
 			{...props}
-			className="fixed flex flex-col gap-3 top-0 inset-x-0 translate-y-0 bg-background/90 backdrop-blur-xs text-foreground p-3 min-h-1/3 max-h-2/3 overflow-y-scroll border-b rounded-b-xl transition-transform duration-300 ease-out data-[starting-style]:-translate-y-full data-[ending-style]:-translate-y-full"
+			className="fixed flex flex-col gap-3 top-0 inset-x-0 translate-y-0 shadow-xl bg-background/90 backdrop-blur-xs text-foreground p-3 min-h-1/3 max-h-2/3 overflow-y-scroll border-b rounded-b-xl transition-transform duration-300 ease-out data-[starting-style]:-translate-y-full data-[ending-style]:-translate-y-full"
 		>
 			{children}
 		</BaseDialog.Popup>
