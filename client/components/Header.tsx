d7998c6b6ebfadc1fbf4ced1e93a5c3279e44453
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-08-15 19:36:52 -0500

    refactor: update Header component to display today's month date in TodayHeader

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,12 +1,11 @@
 import { cx } from "cva";
 import type React from "react";
-import { todayMonthDate } from "../utils/formatDate";
 
 export type HeaderProps = React.ComponentPropsWithoutRef<"h1">;
 
 export const Header = ({ className, children, ...rest }: HeaderProps) => (
 	<h1 className={cx("text-2xl font-bold font-serif", className)} {...rest}>
-		{children ?? todayMonthDate()}
+		{children}
 	</h1>
 );
 
