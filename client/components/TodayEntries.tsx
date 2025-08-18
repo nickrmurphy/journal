b13d7c6cf115d67faa614e8758c650ebde9549e1
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-08-17 22:15:37 -0500

    refactor: improve layout and styling in EntryComments and TodayEntries for better visual consistency

--- client/components/TodayEntries.tsx
+++ client/components/TodayEntries.tsx
@@ -20,11 +20,11 @@ const EntryComments = (props: { entryId: string }) => {
 	);
 
 	return data.length > 0 ? (
-		<div className="px-1 flex flex-col gap-3" ref={parent}>
+		<div className="space-y-2" ref={parent}>
 			{data.map((c) => (
-				<div key={c.id} className="flex gap-1.5 items-start pl-10">
-					<ArrowTurnDownRightIcon className="size-3 text-muted-foreground/40 mt-2 mb-auto min-w-3" />
-					<p className="text-sm text-muted-foreground flex flex-col border border-muted px-2.5 py-2 rounded-md w-full">
+				<div key={c.id} className="flex gap-1.5 items-center">
+					<ArrowTurnDownRightIcon className="size-4 text-muted-foreground/40" />
+					<p className="text-sm text-muted-foreground flex flex-col p-1 w-full">
 						{c.content}
 					</p>
 				</div>
@@ -55,7 +55,7 @@ export const TodayEntries = (props: {
 				<article
 					key={e.id}
 					onClick={() => props.onSelectEntry(e.id)}
-					className="rounded-md p-1 active:bg-white/5 active:brightness-110 space-y-3"
+					className="rounded-md px-2.5 py-2 bg-card border active:brightness-110 space-y-3 text-card-foreground"
 					onKeyDown={(event) => {
 						if (event.key === "Enter") {
 							props.onSelectEntry(e.id);
@@ -63,12 +63,10 @@ export const TodayEntries = (props: {
 					}}
 				>
 					<div className="space-y-1.5">
-						<h2 className="text-xs text-muted-foreground px-1">
+						<h2 className="text-xs text-muted-foreground">
 							{formatTime(e.createdAt)}
 						</h2>
-						<p className="rounded-md bg-card text-card-foreground border px-2.5 py-2 h-full flex items-center">
-							{e.content}
-						</p>
+						<p className="h-full flex items-center">{e.content}</p>
 					</div>
 					<EntryComments entryId={e.id} />
 				</article>
