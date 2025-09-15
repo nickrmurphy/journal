096d085c0792936838e5e47a65212fef5091b917
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-09-15 16:47:20 -0500

    add Button component with variants and styles; update Storybook stories and preview head

--- apps/workshop/src/Button.stories.tsx
+++ apps/workshop/src/Button.stories.tsx
@@ -1,18 +1,35 @@
 import { Button } from "@journal/ui";
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { fn } from "storybook/test";
 
 const meta = {
-	title: "Example/Button",
+	title: "Components/Button",
 	component: Button,
-	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
 	tags: ["autodocs"],
-	parameters: {
-		// More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-		layout: "fullscreen",
-	},
+	args: { onClick: fn(), children: "Button" },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+	args: {},
+};
+
+export const OutlineBlack: Story = {
+	args: {
+		variant: "outline-black",
+	},
+};
+
+export const SolidYellow: Story = {
+	args: {
+		variant: "solid-yellow",
+	},
+};
+
+export const OutlineYellow: Story = {
+	args: {
+		variant: "outline-yellow",
+	},
+};
