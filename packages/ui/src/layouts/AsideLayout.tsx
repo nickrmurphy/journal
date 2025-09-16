9c3f643d3ce073acb2c2e33cf08b7d75e1461a2e
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-09-15 21:14:10 -0500

    refactor: restructure AsideLayout component into separate Root, Aside, and Main exports; update Storybook stories accordingly

--- packages/ui/src/layouts/AsideLayout.tsx
+++ packages/ui/src/layouts/AsideLayout.tsx
@@ -1,11 +1,33 @@
-export const AsideLayout = (props: {
-	aside: React.ReactNode;
-	main: React.ReactNode;
-}) => (
-	<div className="fixed inset-0 grid grid-cols-6">
-		<aside className="col-span-2 overflow-y-auto p-3">{props.aside}</aside>
-		<main className="col-span-4 overflow-y-auto p-3">
-			<div className="mx-auto flex w-full max-w-3xl flex-col">{props.main}</div>
-		</main>
-	</div>
+import { cx } from "cva";
+import type { ComponentProps } from "react";
+
+const Root = (props: ComponentProps<"div">) => (
+	<div
+		{...props}
+		className={cx("fixed inset-0 grid grid-cols-6", props.className)}
+	/>
 );
+
+const Aside = (props: ComponentProps<"aside">) => (
+	<aside
+		{...props}
+		className={cx("col-span-2 overflow-y-auto p-3", props.className)}
+	/>
+);
+
+const Main = (props: ComponentProps<"main">) => (
+	<main
+		{...props}
+		className={cx("col-span-4 overflow-y-auto p-3", props.className)}
+	>
+		<div className="mx-auto flex w-full max-w-3xl flex-col">
+			{props.children}
+		</div>
+	</main>
+);
+
+export const AsideLayout = {
+	Root,
+	Aside,
+	Main,
+};
